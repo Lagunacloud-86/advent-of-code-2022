35f23b34a9f31d61dbe415646a08226935508cda
Lagunacloud <lagcld@gmail.com>
2022-11-30 23:59:20 -0600

    ready for day 2

---
+++ Advent-Of-Code-2022/Day02/Part1.cs
@@ -0,0 +1,42 @@
+using AOCProject;
+using Apt8.Utilities.InputParser.Lib;
+
+namespace Day02;
+
+public sealed class Part1 : IAOCProject<int>
+{
+    private readonly struct Elf
+    {
+        public List<int> FoodCalories { get; }
+
+        public Elf(in InputParser parser, in int index)
+        {
+            FoodCalories = new List<int>();
+            
+            var roValue = parser.GetValue(index);
+            var calories = new InputParser(roValue.ToString(), new CharacterSearcher('\n', true));
+            
+            for (var i = 0; i < calories.NodeCount; ++i)
+                if (int.TryParse(calories.GetValue(i), out var value))
+                    FoodCalories.Add(value);
+        }
+    }
+
+    private readonly List<Elf> _elfs = new ();
+    
+    public void Init(in string input)
+    {
+        var fileData = File.ReadAllText(input);
+        var parser = new InputParser(
+            fileData, 
+            new StringSearcher("\r\n\r\n", true));
+        for (var i = 0; i < parser.NodeCount; i++)
+            _elfs.Add(new Elf(parser, in i));
+    }
+
+    public int Run()
+    {
+        var value = _elfs.Max(x => x.FoodCalories.Sum());
+        return value;
+    }
+}
--- Advent-Of-Code-2022/Day02/Program.cs
+++ Advent-Of-Code-2022/Day02/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AOCProject;
+using Day02;
+
+const string inputPath = "../../../../Inputs";
+
+Console.WriteLine($"The result of day 02 test 1 is: {ProjectRunner.Run<Part1, int>(Path.Combine(inputPath, "day02-test-1.txt"))}");
+Console.WriteLine($"The result of day 02 part 1 is: {ProjectRunner.Run<Part1, int>(Path.Combine(inputPath, "day02-input.txt"))}");
+Console.WriteLine($"The result of day 02 test 2 is: {ProjectRunner.Run<Part2, int>(Path.Combine(inputPath, "day02-test-1.txt"))}");
+Console.WriteLine($"The result of day 02 part 2 is: {ProjectRunner.Run<Part2, int>(Path.Combine(inputPath, "day02-input.txt"))}");
