f4f730a626d3dd2977a54f3f3662907bd0810343
Lagunacloud <lagcld@gmail.com>
2022-12-01 00:00:01 -0600

    cleanup day 1

--- Advent-Of-Code-2022/Day01/Program.cs
+++ Advent-Of-Code-2022/Day01/Program.cs
@@ -4,7 +4,7 @@ using Day01;
 
 const string inputPath = "../../../../Inputs";
 
-Console.WriteLine($"The result of day 01 test 1 is: {ProjectRunner.Run<Test1, int>(Path.Combine(inputPath, "day01-test-1.txt"))}");
+Console.WriteLine($"The result of day 01 test 1 is: {ProjectRunner.Run<Part1, int>(Path.Combine(inputPath, "day01-test-1.txt"))}");
 Console.WriteLine($"The result of day 01 part 1 is: {ProjectRunner.Run<Part1, int>(Path.Combine(inputPath, "day01-input.txt"))}");
-Console.WriteLine($"The result of day 01 test 2 is: {ProjectRunner.Run<Test2, int>(Path.Combine(inputPath, "day01-test-1.txt"))}");
+Console.WriteLine($"The result of day 01 test 2 is: {ProjectRunner.Run<Part2, int>(Path.Combine(inputPath, "day01-test-1.txt"))}");
 Console.WriteLine($"The result of day 01 part 2 is: {ProjectRunner.Run<Part2, int>(Path.Combine(inputPath, "day01-input.txt"))}");
