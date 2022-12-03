414b5be9f5a1c2ecb62126ff92d1899948698b44
Lagunacloud <lagcld@gmail.com>
2022-12-02 23:59:21 -0600

    day 3 finished

--- Advent-Of-Code-2022/Day03/Part1.cs
+++ Advent-Of-Code-2022/Day03/Part1.cs
@@ -5,16 +5,88 @@ namespace Day03;
 
 public sealed class Part1 : IAOCProject<int>
 {
+    public readonly struct Rucksack
+    {
+        private string Contents { get; }
+        public int Length { get; }
+
+        public ReadOnlySpan<char> GetCompartment1()
+        {
+            ReadOnlySpan<char> c = Contents;
+            return c[..Length];
+        }
+        public ReadOnlySpan<char> GetCompartment2()
+        {
+            ReadOnlySpan<char> c = Contents;
+            return c[Length..];
+        }
+
+
+
+        public Rucksack(in ReadOnlySpan<char> section)
+        {
+            Contents = section.ToString().Trim();
+            Length = Contents.Length / 2;
+            
+
+        }
+    }
+
+    private List<Rucksack> _rucksacks = new List<Rucksack>();
+
+    private string _input;
     
     public void Init(in string input)
     {
-        ReadOnlySpan<char> fileContents = File.ReadAllText(input);
-        
+        _input = File.ReadAllText(input);
+        ReadOnlySpan<char> fileContents = _input;
+        var parser = new InputParser(in fileContents, new CharacterSearcher('\n', true));
+        for (var i = 0; i < parser.NodeCount; ++i)
+            _rucksacks.Add(new Rucksack(parser.GetValue(in fileContents, i)));
+
     }
 
     public int Run()
     {
+        var score = 0;
+        foreach (var rs in _rucksacks)
+        {
+            var common = FindCommonItemInRuckSack(in rs);
+            var priority = GetCharPriority(common);
+            score += priority;
+            //Console.WriteLine($"Common: '{common}', Priority: {priority}");
+        }
+
+        //Console.WriteLine($"a: {(int)'a'}, z: {(int)'z'}, A: {(int)'A'}, Z: {(int)'Z'}");
+        
+        
+        return score;
+    }
+
+    private static int GetCharPriority(in char input)
+    {
+        const string lookup = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for(var i = 0; i <lookup.Length; ++i)
+            if (lookup[i] == input)
+                return i + 1;
         return 0;
+    }
+
+    private static char FindCommonItemInRuckSack(in Rucksack rucksack)
+    {
+        var content1 = rucksack.GetCompartment1();
+        var content2 = rucksack.GetCompartment2();
+
+        for (var c1 = 0; c1 < content1.Length; ++c1)
+        {
+            for (var c2 = 0; c2 < content2.Length; ++c2)
+            {
+                if (content1[c1] == content2[c2])
+                    return content1[c1];
+            }
+        }
+
+        return (char)0;
     }
 
 }
--- Advent-Of-Code-2022/Day03/Part2.cs
+++ Advent-Of-Code-2022/Day03/Part2.cs
@@ -5,14 +5,91 @@ namespace Day03;
 
 public sealed class Part2 : IAOCProject<int>
 {
+    private readonly struct Rucksack
+    {
+        public string Contents { get; }
+        public int Length { get; }
+
+        public ReadOnlySpan<char> GetCompartment1()
+        {
+            ReadOnlySpan<char> c = Contents;
+            return c[..Length];
+        }
+        public ReadOnlySpan<char> GetCompartment2()
+        {
+            ReadOnlySpan<char> c = Contents;
+            return c[Length..];
+        }
+
+
+
+        public Rucksack(in ReadOnlySpan<char> section)
+        {
+            Contents = section.ToString().Trim();
+            Length = Contents.Length / 2;
+            
+
+        }
+    }
+
+   
+    private List<Rucksack> _rucksacks = new List<Rucksack>();
+
+    private string _input;
+    
     public void Init(in string input)
     {
-        ReadOnlySpan<char> fileContents = File.ReadAllText(input);
-        
+        _input = File.ReadAllText(input);
+        ReadOnlySpan<char> fileContents = _input;
+        var parser = new InputParser(in fileContents, new CharacterSearcher('\n', true));
+        for (var i = 0; i < parser.NodeCount; ++i)
+            _rucksacks.Add(new Rucksack(parser.GetValue(in fileContents, i)));
+
     }
 
     public int Run()
     {
+        var score = 0;
+        for (var i = 0; i < _rucksacks.Count; i += 3)
+        {
+            var common = FindGroupBadge(_rucksacks[i + 0], _rucksacks[i + 1], _rucksacks[i + 2]);
+            var priority = GetCharPriority(common);
+            score += priority;
+            //Console.WriteLine($"Common: '{common}', Priority: {priority}");
+        }
+        
+        return score;
+    }
+
+    private static int GetCharPriority(in char input)
+    {
+        const string lookup = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for(var i = 0; i <lookup.Length; ++i)
+            if (lookup[i] == input)
+                return i + 1;
         return 0;
+    }
+
+    
+    private static char FindGroupBadge(in Rucksack rucksack1, in Rucksack rucksack2, in Rucksack rucksack3)
+    {
+        var status = new Dictionary<char, byte>(52);
+        CountGroupBadge(status, rucksack1.Contents, 0b001);
+        CountGroupBadge(status, rucksack2.Contents, 0b010);
+        CountGroupBadge(status, rucksack3.Contents, 0b100);
+        
+        return status.First(x => x.Value == 0b111).Key;
+    }
+
+    private static void CountGroupBadge(Dictionary<char, byte> status, in ReadOnlySpan<char> content, in byte bit)
+    {
+        for (var i = 0; i < content.Length; ++i)
+        {
+            if (!status.ContainsKey(content[i]))
+                status.Add(content[i], 0);
+
+            status[content[i]] |= bit;
+        }
+
     }
 }
